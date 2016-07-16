36c31e33361f4778ab4e4783629185213b608ca2
tianganghuti <491574452@qq.com>
2016-07-16 16:58:53 +0800

    run unit test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -23,28 +23,4 @@ public class HelloWorldTest {
         PurchaseItem item = discount.ParseOneItem("ITEM000001-2");
         assertEquals(item.getRealprice(), 196, 0.1);
     }
-
-//  @org.junit.Test
-//   public void should_return_totallfee_cocola() throws Exception {
-//        assertTrue(new Cocola(COCOLA_NUM,COCOLA_PRICE).getTotalFee().equals(new BigDecimal(9)));
-//   }
-//    @org.junit.Test
-//    public void should_return_discountfee_cocola() throws Exception {
-//        assertTrue(new Cocola(COCOLA_NUM,COCOLA_PRICE).getDiscountFee(FIVE_PERCENT_DISCOUNT).equals(new BigDecimal(0.45)));
-//    }
-
-    //羽毛球
-//    @org.junit.Test
-//    public void should_return_totallfee_badminton() throws Exception {
-//        assertTrue(new Badminton(BADMINTON_NUM,BADMINTON_PRICE).getTotalFee().equals(new BigDecimal(5)));
-//    }
-//    @org.junit.Test
-//    public void should_return_discountfee_badminton() throws Exception {
-//        assertTrue(new Badminton(COCOLA_NUM,COCOLA_PRICE).getDiscountFee(TEN_PERCENT_DICOUNT).equals(new BigDecimal(0.5)));
-//    }
-
-//    @org.junit.Test
-//    public void should_return_discountfee_apple() throws Exception {
-//        assertTrue(new Badminton(COCOLA_NUM,COCOLA_PRICE).getDiscountFee(NO_DISCOUNT).equals(new BigDecimal(0.5)));
-//    }
 }
